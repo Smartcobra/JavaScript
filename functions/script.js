f714884697ccd861f233a4f9794deaea8037df9a
jitu <iamjitu.pradhan@gmail..com>
2021-09-19 12:52:49 +0530

    functiontype

--- functions/script.js
+++ functions/script.js
@@ -8,13 +8,31 @@
 // if(hasDriversLiences) console.log("I can drive");
 
 
-function fruitMixer(apple,orange){
-    console.log("In fruit mixer");
-    const juice=`juice of ${apple} apples and ${orange} oranges`;
-    return juice;
+// function fruitMixer(apple,orange){
+//     console.log("In fruit mixer");
+//     const juice=`juice of ${apple} apples and ${orange} oranges`;
+//     return juice;
 
+// }
+
+// const juiceType=fruitMixer(3,0);
+// console.log(juiceType);
+// console.log("hi");
+
+///////function declaration and function expression
+
+
+///function in a declarative way
+function getAge(birthYear){
+  return 2037-birthYear;
 }
 
-const juiceType=fruitMixer(3,0);
-console.log(juiceType);
-console.log("hi");
+const age1=getAge(1991);
+//function expression ---anonymous function
+
+const getAge2=function(birthYear){
+ return 2037-birthYear;
+}
+const age2= getAge2(1991);
+
+console.log(age1,age2);
