25a116af1f3bce9f747e7c5ae88fe6fd76ff53b7
jitu <iamjitu.pradhan@gmail..com>
2021-10-06 23:13:23 +0530

    refactoring

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -9,8 +9,11 @@
 
 let secretNumber=Math.trunc(Math.random()*20)+1;
 let score=20;
+let higScore=0;
 
-
+const displayMessage= function(message){
+document.querySelector('.message').textContent=message;
+}
 
 document.querySelector('.check').addEventListener('click',
 function(){
@@ -20,29 +23,48 @@ function(){
         document.querySelector('.message').textContent= '❌ :: No Number ';
         //when palyer wins
     }else if(guess===secretNumber){
-         document.querySelector('.message').textContent= '👏 Correct Number ';
+        // document.querySelector('.message').textContent= '👏 Correct Number ';
+        displayMessage('👏 Correct Number ');
          document.querySelector('.number').textContent=secretNumber;
          document.querySelector('body').style.backgroundColor= '#60b347';
          document.querySelector('.number').style.width='30rem';
-    }else if(guess > secretNumber){
-      if(score > 0){
-          document.querySelector('.message').textContent= '😇  Large number';
-         score--;
-         document.querySelector('.score').textContent= score;
+
+         if(score>higScore){
+           higScore=score;
+           document.querySelector('.highscore').textContent=higScore;
+         }
+    }else if(guess!==secretNumber){
+         if(score > 1){
+           document.querySelector('.message').textContent=
+           guess>secretNumber ? '😇  Large number' :'😫  Small number'
+            score--;
+            document.querySelector('.score').textContent= score;
       }else{
-        document.querySelector('.message').textContent= '🤯 You lost the game';
+        //document.querySelector('.message').textContent= '🤯 You lost the game';
+        displayMessage('🤯 You lost the game');
+         document.querySelector('body').style.backgroundColor= '#FF0000';
+        document.querySelector('.score').textContent=0;
       }
-        
-    }else if (guess < secretNumber){
-      if(score > 0){
-              document.querySelector('.message').textContent= '😫  Small number';
-             score--;
-              document.querySelector('.score').textContent= score;
-      }else{
-        document.querySelector('.message').textContent= '🤯 You lost the game';
-      }
-            
     }
+    // else if(guess > secretNumber){
+    //   if(score > 0){
+    //       document.querySelector('.message').textContent= '😇  Large number';
+    //      score--;
+    //      document.querySelector('.score').textContent= score;
+    //   }else{
+    //     document.querySelector('.message').textContent= '🤯 You lost the game';
+    //   }
+        
+    // }else if (guess < secretNumber){
+    //   if(score > 0){
+    //           document.querySelector('.message').textContent= '😫  Small number';
+    //          score--;
+    //           document.querySelector('.score').textContent= score;
+    //   }else{
+    //     document.querySelector('.message').textContent= '🤯 You lost the game';
+    //   }
+            
+    // }
     
 })
 
